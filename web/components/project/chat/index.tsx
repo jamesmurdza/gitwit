77acbe2474833306dd1feb79b9ee77afac561664
Hamzat Victor <oluwaborihamzat@gmail.com>
2025-10-22 22:23:26 +0200

    fix: increase ChatScrollContainer width for improved layout

--- web/components/project/chat/index.tsx
+++ web/components/project/chat/index.tsx
@@ -51,7 +51,7 @@ function MainChatContent() {
     return <ChatContainerEmpty />
   }
   return (
-    <ChatScrollContainer className="flex-1 relative w-full max-w-4xl mx-auto">
+    <ChatScrollContainer className="flex-1 relative w-full max-w-5xl mx-auto">
       <ChatContainerContent className="px-2 py-4  overflow-x-hidden">
         {messages.map((message, i) => {
           return (
